5be7ccdec505aad8cc24e5a8df07d7eb29281049
samuxlchamorro <csamuelcaled@gmail.com>
2026-08-27 13:36:50 -0500

    Add age calculation based on user input

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+
+nombre = input("Ingrese su nombre: ")
+anio_nacimiento = int(input("Ingrese su año de nacimiento: "))
+anio_actual = int(input("Ingrese el año actual: "))
+
+edad = anio_actual - anio_nacimiento
+
+print(f"{nombre} tiene aproximadamente {edad} años.")
