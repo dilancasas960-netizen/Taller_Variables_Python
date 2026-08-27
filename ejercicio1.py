343e6952b0cbe0cfa867d495d1d153145304e310
sesinfonia-ux <sesinfonia@gmail.com>
2026-08-27 15:52:32 -0500

    ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+nombre = "Sebastian Arismendi"
+edad = 19
+semestre = 2
+programa = "Ingeniería de Sistemas"
+promedio = 4.7
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Semestre:", semestre)
+print("Programa:", programa)
+print("Promedio:", promedio)
