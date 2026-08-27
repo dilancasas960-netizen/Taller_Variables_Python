8c534383df4cd71cd034e37200d54d1b0670a9cf
samuxlchamorro <csamuelcaled@gmail.com>
2026-08-27 13:40:29 -0500

    Add currency conversion script from dollars to pesos

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+
+Ejercicio 10. Conversión de moneda
+print("ingrese una cantidad de dinero en dolares: ")
+tasa_cambio = 3154.76
+dolares = float(input())
+print(f"{dolares} dolares son {dolares * tasa_cambio} pesos colombianos.")
+
